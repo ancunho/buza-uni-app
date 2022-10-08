0ad6b795aa4c9558eca18d77a0b1f7d478a633d6
cunho <ancunho@naver.com>
2022-10-08 22:59:02 +0800

    youtr -> intro

--- buza-uni-app/config.js
+++ buza-uni-app/config.js
@@ -1,8 +1,8 @@
 export default {
 	application_name: 'Buza',
-	// app_web_url: 'http://localhost:8001/manage', // h5域名PC域名，用于分享图片或者拼接图片时使用，结尾必须加 “/”
-	app_web_url: 'https://www.cunho.online/manage', // h5域名PC域名，用于分享图片或者拼接图片时使用，结尾必须加 “/”
-	// app_web_api_url: 'http://localhost:8001/manage',
-	app_web_api_url: 'https://www.cunho.online/manage',
+	app_web_url: 'http://localhost:8001/manage', // h5域名PC域名，用于分享图片或者拼接图片时使用，结尾必须加 “/”
+	// app_web_url: 'https://www.cunho.online/manage', // h5域名PC域名，用于分享图片或者拼接图片时使用，结尾必须加 “/”
+	app_web_api_url: 'http://localhost:8001/manage',
+	// app_web_api_url: 'https://www.cunho.online/manage',
 	debug: true, 
 }
